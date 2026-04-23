be0886df21481cb88f18ff37a9d83c97b4ca4d64
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-04-23 21:28:40 +0300

    Error fixed

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -4,7 +4,7 @@ from app.restore_names import restore_names
 def test_restore_names_updates_missing_and_none() -> None:
     users = [
         {"first_name": None, "last_name": "Holy", "full_name": "Jack Holy"},
-        {"last_name": "Adams", "full_name": "Mike Adams"},
+        {"last_name": "Dams", "full_name": "Mike Dams"},
         {"first_name": "John", "last_name": "Doe", "full_name": "John Doe"}
     ]
     restore_names(users)
