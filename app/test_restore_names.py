9a3eefd5d442db237736cab03ec200ba3d96cac3
Yevgeniy Marchenko <Jackson.stethem@gmail.com>
2026-04-23 21:26:59 +0300

    Implement test for restore_names

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -11,6 +11,6 @@ def test_restore_names_updates_missing_and_none() -> None:
 
     assert users == [
         {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"},
-        {"first_name": "Mike", "last_name": "Adams", "full_name": "Mike Adams"},
+        {"first_name": "Mike", "last_name": "Dams", "full_name": "Mike Dams"},
         {"first_name": "John", "last_name": "Doe", "full_name": "John Doe"}
     ]
